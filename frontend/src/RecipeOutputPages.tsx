7ebe280903441bb6923ddacac05cf25fb74bb966
MithosRiot <65834550+MithosRiot@users.noreply.github.com>
2026-08-30 18:29:47 -0500

    feat: wrap recipe pages with outputs panel

---
+++ frontend/src/RecipeOutputPages.tsx
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom'
+import { fetchRecipe } from './api'
+import { RecipeDetailWithVariantsPage, RecipeEditorWithVariantsPage } from './RecipeVariantPages'
+import RecipeOutputsPanel from './RecipeOutputsPanel'
+
+function OutputsPanel() {
+  const { recipeId } = useParams()
+  const id = Number(recipeId)
+  const recipe = useQuery({ queryKey: ['recipe', id], queryFn: () => fetchRecipe(id), enabled: Number.isFinite(id) })
+  if (!recipe.data) return null
+  return <div style={{ marginTop: 20 }}><RecipeOutputsPanel recipe={recipe.data} /></div>
+}
+
+export function RecipeEditorWithAdvancedPage() {
+  return <><RecipeEditorWithVariantsPage /><OutputsPanel /></>
+}
+
+export function RecipeDetailWithAdvancedPage() {
+  return <><RecipeDetailWithVariantsPage /><OutputsPanel /></>
+}
